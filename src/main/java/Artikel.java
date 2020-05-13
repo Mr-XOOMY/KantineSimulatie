4eebaeef7934895afeb211cf64932fb5b6a99c2d
Anonymity <myrmidon@protonmail.ch>
2020-05-13 17:27:38 +0200

    Klasse Artikel

---
+++ src/main/java/Artikel.java
@@ -0,0 +1,53 @@
+public class Artikel {
+    private String naam;
+    private float prijs;
+
+    /**
+     * Constructor
+     * @param naam Artikelnaam
+     * @param prijs Artikelprijs
+     */
+    public Artikel(String naam, float prijs) {
+        this.naam = naam;
+        this.prijs = prijs;
+    }
+
+    /**
+     * Parameterloze Constructor
+     */
+    public Artikel() {
+
+    }
+
+    /**
+     * Getter voor naam Artikel
+     * @return Artikelnaam
+     */
+    public String getNaam() {
+        return naam;
+    }
+
+    /**
+     * Getter voor prijs Artikel
+     * @return Artikelprijs
+     */
+    public float getPrijs() {
+        return prijs;
+    }
+
+    /**
+     * Setter voor naam Artikel
+     * @param naam Ingegeven Artikelnaam
+     */
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    /**
+     * Setter voor prijs Artikel
+     * @param prijs Ingegeven Artikelprijs
+     */
+    public void setPrijs(float prijs) {
+        this.prijs = prijs;
+    }
+}
