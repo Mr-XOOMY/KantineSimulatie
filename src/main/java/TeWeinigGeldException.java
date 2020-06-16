51665e080f1ee10b202c0fcbab36b840b6733dda
Anonymity <myrmidon@protonmail.ch>
2020-06-16 10:04:18 +0200

    Week 4 Opgave 5 Exceptions

---
+++ src/main/java/TeWeinigGeldException.java
@@ -0,0 +1,18 @@
+public class TeWeinigGeldException extends RuntimeException {
+
+
+    public TeWeinigGeldException() {
+
+    }
+
+
+    public TeWeinigGeldException(Exception e) {
+
+    }
+
+
+    public TeWeinigGeldException(String message) {
+
+    }
+
+}
